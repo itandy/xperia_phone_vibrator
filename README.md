e3935ae78d1329deb3b800a2955398dee3c370d8
Andy Wong <andy@gzplanet.com>
2013-10-04 22:31:22 +0800

    Rewritten based on AOSP ICS/JB source code

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-Xperia Phone Vibrator
+Android Phone Vibrator
 ===================================
 
-Vibrator your Sony Xperia phone when an outgoing call is connected and ended
+Vibrator your Android phone when an outgoing call is connected and ended
 
-This is a Xposed module so your phone must be rooted to use it. The module is based on latest Xperia phone app. I tested it using my Sony Xperia ZR phone with 4.2.2 stock ROM.
+This is a Xposed module so your phone must be rooted to use it. The module is based on AOSP ICS/JB phone app. I tested it using my Sony Xperia ZR phone with 4.2.2 stock ROM.
 
 Installation instructions:
 1. Install the module
-2. Run Xposed Installer and enable the module Vibrate on connected/ended outgoing call for Xperia phones
+2. Run Xposed Installer and enable the module Vibrate on connected outgoing call or ended call for Android phones
 3. Reboot the phone
 
--- src/com/gzplanet/xposed/xperiaphonevibrator/XperiaPhoneVibrator.java
+++ src/com/gzplanet/xposed/xperiaphonevibrator/XperiaPhoneVibrator.java
@@ -2,7 +2,14 @@ package com.gzplanet.xposed.xperiaphonevibrator;
 
 import static de.robv.android.xposed.XposedHelpers.getObjectField;
 import android.content.Context;
+import android.os.AsyncResult;
 import android.os.Vibrator;
+
+import com.android.internal.telephony.Call;
+import com.android.internal.telephony.CallManager;
+import com.android.internal.telephony.Connection;
+import com.android.internal.telephony.Phone;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
@@ -11,39 +18,79 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class XperiaPhoneVibrator implements IXposedHookLoadPackage {
 	final static String PKGNAME_SETTINGS = "com.android.phone";
+	final long[] patternConnected = new long[] { 0, 100, 0, 0 };
+	final long[] patternEnded = new long[] { 0, 50, 100, 50 };
 
 	@Override
-	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
 		if (!lpparam.packageName.equals(PKGNAME_SETTINGS))
 			return;
 
-		String mFuncName = null;
+		XposedHelpers.findAndHookMethod(PKGNAME_SETTINGS + ".CallNotifier", lpparam.classLoader, "onPhoneStateChanged",
+				AsyncResult.class, new XC_MethodHook() {
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						CallManager mCM = (CallManager) getObjectField(param.thisObject, "mCM");
 
-		try {
-			if (XposedHelpers.findMethodExact(PKGNAME_SETTINGS + ".LargeCallView", lpparam.classLoader,
-					"hideCallingProgress") != null)
-				mFuncName = "hideCallingProgress";
-		} catch (java.lang.NoSuchMethodError e) {
-			XposedBridge.log("hideCallingProgress not found");
-			if (XposedHelpers.findMethodExact(PKGNAME_SETTINGS + ".LargeCallView", lpparam.classLoader,
-					"hideConnectingStatus") != null)
-				mFuncName = "hideConnectingStatus";
-			XposedBridge.log("hideConnectingStatus found");
-		}
-
-		XposedBridge.log("mFuncName: " + mFuncName);
-		if (mFuncName != null)
-			XposedHelpers.findAndHookMethod(PKGNAME_SETTINGS + ".LargeCallView", lpparam.classLoader, mFuncName,
-					new XC_MethodHook() {
-						@Override
-						protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-							Context context = (Context) getObjectField(param.thisObject, "mContext");
-							XposedBridge.log("context: " + context.hashCode());
-
-							Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-							vibrator.vibrate(150);
+						Object state = XposedHelpers.callMethod(mCM, "getState");
+//						XposedBridge.log("state: " + state.toString());
+						if (state.toString().equals("OFFHOOK")) {
+							Phone fgPhone = mCM.getFgPhone();
+							Call call = getCurrentCall(fgPhone);
+							Connection c = getConnection(fgPhone, call);
+							if (c != null) {
+								Call.State cstate = call.getState();
+/*								XposedBridge.log(String.format("cstate:%d isIncoming:%b durationMillis:%d",
+										cstate.ordinal(), c.isIncoming(), c.getDurationMillis()));
+*/
+								if (cstate == Call.State.ACTIVE && !c.isIncoming() && c.getDurationMillis() < 200)
+									vibratePhone(param.thisObject, patternConnected);
+							}
 						}
-					});
+					}
+				});
+
+		XposedHelpers.findAndHookMethod(PKGNAME_SETTINGS + ".CallNotifier", lpparam.classLoader, "onDisconnect",
+				AsyncResult.class, new XC_MethodHook() {
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						Connection c = (Connection) ((AsyncResult) param.args[0]).result;
+						if (c != null && c.getDurationMillis() > 0)
+							vibratePhone(param.thisObject, patternEnded);
+					}
+				});
 	}
 
+	void vibratePhone(Object thisObject, long[] pattern) {
+		Context app = (Context) getObjectField(thisObject, "mApplication");
+		Vibrator vibrator = (Vibrator) app.getSystemService(Context.VIBRATOR_SERVICE);
+		vibrator.vibrate(pattern, -1);
+	}
+
+	// static helper functions from CM
+	static Call getCurrentCall(Phone phone) {
+		Call ringing = phone.getRingingCall();
+		Call fg = phone.getForegroundCall();
+		Call bg = phone.getBackgroundCall();
+		if (!ringing.isIdle()) {
+			return ringing;
+		}
+		if (!fg.isIdle()) {
+			return fg;
+		}
+		if (!bg.isIdle()) {
+			return bg;
+		}
+		return fg;
+	}
+
+	static Connection getConnection(Phone phone, Call call) {
+		if (call == null) {
+			return null;
+		}
+		if (phone.getPhoneType() == Phone.PHONE_TYPE_CDMA) {
+			return call.getLatestConnection();
+		}
+		return call.getEarliestConnection();
+	}
 }
