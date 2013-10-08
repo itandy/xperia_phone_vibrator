f8f5c0550ec7f1287b6d47d4810f505535760fcb
Andy Wong <andy@gzplanet.com>
2013-10-08 22:17:23 +0800

    New options for incoming calls and preference screen to turn on/off features

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 Android Phone Vibrator
 ===================================
 
-Vibrator your Android phone when an outgoing call is connected and ended
+Vibrator your Android phone on different call status.
+
+Features:
+1. Vibrate when an outgoing call is connected
+2. Vibrate when an incoming call is connected
+3. Vibrate when an incoming call is waiting
+4. Vibrate when a call is ended
+5. Any of the above features can be turned on/off
 
 This is a Xposed module so your phone must be rooted to use it. The module is based on AOSP ICS/JB phone app. I tested it using my Sony Xperia ZR phone with 4.2.2 stock ROM.
 
--- src/com/gzplanet/xposed/xperiaphonevibrator/XperiaPhoneVibrator.java
+++ src/com/gzplanet/xposed/xperiaphonevibrator/XperiaPhoneVibrator.java
@@ -11,52 +11,89 @@ import com.android.internal.telephony.Connection;
 import com.android.internal.telephony.Phone;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class XperiaPhoneVibrator implements IXposedHookLoadPackage {
+public class XperiaPhoneVibrator implements IXposedHookZygoteInit, IXposedHookLoadPackage {
 	final static String PKGNAME_SETTINGS = "com.android.phone";
 	final long[] patternConnected = new long[] { 0, 100, 0, 0 };
-	final long[] patternEnded = new long[] { 0, 50, 100, 50 };
+	final long[] patternHangup = new long[] { 0, 50, 100, 50 };
+	final long[] patternCallWaiting = new long[] { 0, 200, 300, 500 };
+
+	private static XSharedPreferences pref;
+
+	@Override
+	public void initZygote(StartupParam startupParam) throws Throwable {
+		pref = new XSharedPreferences(XperiaPhoneVibrator.class.getPackage().getName());
+		// just in case the preference file permission is reset by recovery/script
+		pref.makeWorldReadable();
+	}
 
 	@Override
 	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
 		if (!lpparam.packageName.equals(PKGNAME_SETTINGS))
 			return;
 
+		// hook onPhoneStateChanged for outgoing calls
 		XposedHelpers.findAndHookMethod(PKGNAME_SETTINGS + ".CallNotifier", lpparam.classLoader, "onPhoneStateChanged",
 				AsyncResult.class, new XC_MethodHook() {
 					@Override
 					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						pref.reload();
 						CallManager mCM = (CallManager) getObjectField(param.thisObject, "mCM");
 
 						Object state = XposedHelpers.callMethod(mCM, "getState");
-//						XposedBridge.log("state: " + state.toString());
+						// XposedBridge.log("state: " + state.toString());
 						if (state.toString().equals("OFFHOOK")) {
 							Phone fgPhone = mCM.getFgPhone();
 							Call call = getCurrentCall(fgPhone);
 							Connection c = getConnection(fgPhone, call);
 							if (c != null) {
 								Call.State cstate = call.getState();
-/*								XposedBridge.log(String.format("cstate:%d isIncoming:%b durationMillis:%d",
-										cstate.ordinal(), c.isIncoming(), c.getDurationMillis()));
-*/
-								if (cstate == Call.State.ACTIVE && !c.isIncoming() && c.getDurationMillis() < 200)
-									vibratePhone(param.thisObject, patternConnected);
+
+//								XposedBridge.log(String.format("cstate:%d isIncoming:%b durationMillis:%d",
+//										cstate.ordinal(), c.isIncoming(), c.getDurationMillis()));
+
+								if (cstate == Call.State.ACTIVE && c.getDurationMillis() < 200)
+									if (pref.getBoolean("pref_vibrate_outgoing", true) && !c.isIncoming()
+											|| pref.getBoolean("pref_vibrate_incoming", true) && c.isIncoming())
+										vibratePhone(param.thisObject, patternConnected);
 							}
 						}
 					}
 				});
 
+		// hook onDisconnect for disconnected calls
 		XposedHelpers.findAndHookMethod(PKGNAME_SETTINGS + ".CallNotifier", lpparam.classLoader, "onDisconnect",
 				AsyncResult.class, new XC_MethodHook() {
 					@Override
 					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-						Connection c = (Connection) ((AsyncResult) param.args[0]).result;
-						if (c != null && c.getDurationMillis() > 0)
-							vibratePhone(param.thisObject, patternEnded);
+						pref.reload();
+						if (pref.getBoolean("pref_vibrate_hangup", true)) {
+							Connection c = (Connection) ((AsyncResult) param.args[0]).result;
+							if (c != null && c.getDurationMillis() > 0)
+								vibratePhone(param.thisObject, patternHangup);
+						}
+					}
+				});
+
+		// hook onNewRingingConnection for new call waiting
+		XposedHelpers.findAndHookMethod(PKGNAME_SETTINGS + ".CallNotifier", lpparam.classLoader,
+				"onNewRingingConnection", AsyncResult.class, new XC_MethodHook() {
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						pref.reload();
+						if (pref.getBoolean("pref_vibrate_call_waiting", true)) {
+							CallManager cm = (CallManager) getObjectField(param.thisObject, "mCM");
+							Connection c = (Connection) ((AsyncResult) param.args[0]).result;
+							Call.State state = c.getState();
+
+							if (!isRealIncomingCall(state, cm))
+								vibratePhone(param.thisObject, patternCallWaiting);
+						}
 					}
 				});
 	}
@@ -67,7 +104,7 @@ public class XperiaPhoneVibrator implements IXposedHookLoadPackage {
 		vibrator.vibrate(pattern, -1);
 	}
 
-	// static helper functions from CM
+	// static helper functions from CM PhoneUtils class
 	static Call getCurrentCall(Phone phone) {
 		Call ringing = phone.getRingingCall();
 		Call fg = phone.getForegroundCall();
@@ -92,5 +129,9 @@ public class XperiaPhoneVibrator implements IXposedHookLoadPackage {
 			return call.getLatestConnection();
 		}
 		return call.getEarliestConnection();
+	}
+
+	static boolean isRealIncomingCall(Call.State state, CallManager cm) {
+		return (state == Call.State.INCOMING && !cm.hasActiveFgCall());
 	}
 }
